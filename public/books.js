87618e4de6676751b3a499f5667bd1937c0f9b4f
Margarita <m_mg489@outlook.com>
2019-07-13 23:55:43 -0400

    book data structure defined for schema

--- public/books.js
+++ public/books.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+//DB's data Structure
+
+const BookSchema = new Schema({
+    title:String,
+    authors: String,
+    rating: Number, 
+    publisher: String,
+    publishedDate: String, 
+    description: String,
+    thumbnail: String,
+    price: Number,
+    purchase: String,
+});
