3aa9ba5dee905b27cbe7ee23c899a21348f31a73
Margarita <m_mg489@outlook.com>
2019-07-17 14:23:39 -0400

    routes get

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 const axios = require('axios');
 const Book = require('../../models/book');
+
+router.get('/', (req,res)=>{
+    Book.find()
+        .then(booksData => {
+            res.json(booksData);
+        })
+        .catch(err=>{
+            res.json({error: err});
+        });
+});
