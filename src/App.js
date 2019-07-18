571afba2eb9c5fd3700e8e2558f437c0e9cd6257
Margarita <m_mg489@outlook.com>
2019-07-18 00:47:16 -0400

    app.js updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
+import Books from './pages/Books';
+import Saved from './pages/Saved';
+import NavMenu from './components/NavMenu';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='application'>
+        <NavMenu />
+        <main className='main'>
+          <Container>
+            <div className='wrapper'>
+              <Switch>
+                <Route exact path='/' component={Books} />
+                <Route path='/saved' component={Saved} />
+              </Switch>
+            </div>
+          </Container>
+        </main>
+      </div>
+    </Router>
   );
 }
 
